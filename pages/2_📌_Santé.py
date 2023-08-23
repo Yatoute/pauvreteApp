20cfc6806579a64341d6d256774940b6a65e2dff
Yatoute MINTOAMA <100260500+Yatoute@users.noreply.github.com>
2023-08-23 03:51:12 +0000

    Update 2_📌_Santé.py

--- pages/2_📌_Santé.py
+++ pages/2_📌_Santé.py
@@ -36,22 +36,22 @@ def sante_page():
      st.markdown(sect3, unsafe_allow_html=True)
      st.write("")
      ## Nombre de consultation
-     Nb_Consult = st.number_input("➡️ Combien de personnes dans le ménage, ont consulté un service de santé au cours des 30 derniers jours ?" , 0, None, data.get('Nb_Consult'))
+     Nb_Consult = st.number_input("🏷️ Combien de personnes dans le ménage, ont consulté un service de santé au cours des 30 derniers jours ?" , 0, None, data.get('Nb_Consult'))
      st.write("")
      ## Satisfaction des soins de santé
      if Nb_Consult > 0 :
-         Satisfait_y = st.number_input("➡️ Parmi les personnes ayant effectuer une consultation, combien sont satisfaits des services sanitaires lors de leur consultations ?" , 0, None, data.get('Satisfait_y'))
+         Satisfait_y = st.number_input("🏷️ Parmi les personnes ayant effectuer une consultation, combien sont satisfaits des services sanitaires lors de leur consultations ?" , 0, None, data.get('Satisfait_y'))
          st.write("")
      else :
          Satisfait_y =0
      ## Couverture maladie
-     Couv_Maladi_y = st.number_input("➡️ Combien de personnes dans le ménage, disposent d’une assurance maladie ou bénéficient d’une prise en charge particulière ?" , 0, None, data.get('Couv_Maladi_y'))
+     Couv_Maladi_y = st.number_input("🏷️ Combien de personnes dans le ménage, disposent d’une assurance maladie ou bénéficient d’une prise en charge particulière ?" , 0, None, data.get('Couv_Maladi_y'))
      st.write("")
      ## Maladies chroniques
-     Chroniq_y = st.number_input("➡️ Combien de personnes dans le ménage, souffrent d'une maladie chronique ?" , 0, None, data.get('Chroniq_y'))
+     Chroniq_y = st.number_input("🏷️ Combien de personnes dans le ménage, souffrent d'une maladie chronique ?" , 0, None, data.get('Chroniq_y'))
      st.write("")
      ## Indicape
-     Handicap_y = st.number_input("➡️ Combien de personnes dans le ménage, souffrent d'un handicap physique ou mental ?" , 0, None, data.get('Handicap_y'))
+     Handicap_y = st.number_input("🏷️ Combien de personnes dans le ménage, souffrent d'un handicap physique ou mental ?" , 0, None, data.get('Handicap_y'))
      st.write("")
     # Enregistrer les données dans les cookies ou la session
      st.session_state.saved_data.update(init_session_sante(Nb_Consult, Satisfait_y, Couv_Maladi_y, Chroniq_y, Handicap_y))
